93aed6f964cc2978400b42efc307ab9a498d7650
Colsrch <colsrch@foxmail.com>
2022-02-24 09:28:18 +0800

    Fix bug, 优化代码 (#3)
    * Fix bug, 优化代码
    
    * Update v1.py

--- generator/run.py
+++ generator/run.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 # author: https://github.com/Zfour
-import requests
-from bs4 import BeautifulSoup
-import re
-import yaml
-from request_data import request
 import json
+
+import yaml
+from bs4 import BeautifulSoup
+
+from request_data import request
 
 data_pool = []
 
+
 def load_config():
-    f = open('_config.yml', 'r',encoding='utf-8')
+    f = open('_config.yml', 'r', encoding='utf-8')
     ystr = f.read()
     ymllist = yaml.load(ystr, Loader=yaml.FullLoader)
     return ymllist
 
-def github_issuse(data_pool):
+
+def github_issuse():
     print('\n')
     print('------- github issues start ----------')
     baselink = 'https://github.com/'
@@ -27,11 +29,10 @@ def github_issuse(data_pool):
                 label_plus = '+label%3A' + config['issues']['label']
             else:
                 label_plus = ''
-            github = request.get_data('https://github.com/' +
-                             config['issues']['repo'] +
-                             '/issues?q=is%3A' + config['issues']['state'] + str(label_plus) + '&page=' + str(number))
+            github = request.get_data(
+                f"https://github.com/{config['issues']['repo']}/issues?q=is%3A{config['issues']['state']}{str(label_plus)}&page={str(number)}")
             soup = BeautifulSoup(github, 'html.parser')
-            main_content = soup.find_all('div',{'aria-label': 'Issues'})
+            main_content = soup.find_all('div', {'aria-label': 'Issues'})
             linklist = main_content[0].find_all('a', {'class': 'Link--primary'})
             if len(linklist) == 0:
                 print('> end')
@@ -49,15 +50,15 @@ def github_issuse(data_pool):
                         data_pool.append(source)
                 except:
                     continue
-    except Exception as e:
+    except:
         print('> end')
 
     print('------- github issues end ----------')
     print('\n')
 
 
-#友链规则
-github_issuse(data_pool)
-filename='generator/output/v1/data.json'
-with open(filename,'w',encoding='utf-8') as file_obj:
-   json.dump(data_pool,file_obj,ensure_ascii=False)
+# 友链规则
+github_issuse()
+filename = 'generator/output/v1/data.json'
+with open(filename, 'w', encoding='utf-8') as file_obj:
+    json.dump(data_pool, file_obj, ensure_ascii=False)
